fcf3586d30d990af028dda37e5b15ce2ba3378a0
RpnAndri <andri.crisostomo@gmail.com>
2025-05-07 15:43:52 +0900

    add: subclass

--- ex6/script.js
+++ ex6/script.js
@@ -1,19 +1,29 @@
-function createCounter() {
-    let count = 0;
-    return {
-        increment: function() {
-                count++;
-                // Update the content of the HTML element to display the current count
-                document.getElementById("counterDisplay").innerText = "Count: " + count;
-            }
-        };
-
+class Car {
+    constructor(make, model, year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+    start() {
+        console.log(`${this.make} ${this.model} started.`);
+    }
 }
-const counter = createCounter();
 
-    // Get reference to the button and set up the event listener
-document.getElementById("incrementButton").addEventListener("click", function() {
-    counter.increment();
-});
+// Creating instances of Car
+const car1 = new Car('Toyota', 'Camry', 2020);
+const car2 = new Car('Honda', 'Accord', 2021);
+car1.start(); // Should log "Toyota Camry started."
+car2.start(); // Should log "Honda Accord started."
 
-
+class ElectricCar extends Car {
+    constructor(make, model, year, batteryCapacity) {
+        super(make, model, year); // Call the parent class constructor
+        this.batteryCapacity = batteryCapacity;
+    }
+    charge() {
+        console.log(`${this.make} ${this.model} is charging.`);
+    }
+}
+    const tesla = new ElectricCar('Tesla', 'Model S', 2022, '100 kWh');
+    tesla.start(); // Should log "Tesla Model S started."
+    tesla.charge(); // Should log "Tesla Model S is charging."
