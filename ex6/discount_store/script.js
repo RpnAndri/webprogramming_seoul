582a6e2c4029d4ef5cd96fccffe7c30b3ceec2d1
RpnAndri <andri.crisostomo@gmail.com>
2025-05-07 17:16:38 +0900

    done: task 1

--- ex6/discount_store/script.js
+++ ex6/discount_store/script.js
@@ -1,1 +1,15 @@
-base_price = 0;
+// Get the base price as a global variable
+base_price = Number(document.getElementById("price").textContent);
+DISCOUNT_PERCENTAGE = 20;
+// discountPercentage is an integer which represents the percentage.
+function createDiscount(discountPercentage) {
+
+    function inner() {
+        document.getElementById("discountedPrice").textContent = base_price * (1-(discountPercentage*0.01));
+    }
+    
+    return inner;
+}
+
+document.getElementById("applyDiscount").addEventListener("click", createDiscount(DISCOUNT_PERCENTAGE));
+
